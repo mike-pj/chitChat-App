f6579afe261767972b3a2c3511599edc472da6ef
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-03-02 21:28:35 +0100

    seting frontend environment and installing packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser"
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -20,9 +21,12 @@ mongoose
     console.log("Error connecting to DB");
   });
 
-app.use(cookieParser());
-
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
