442e0c6d68562f6ed4b2ddf5b168ee47798320dc
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-03-10 13:04:54 +0100

    Settings page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from './pages/ProfilePage';
 
 import { Routes, Route, Navigate } from "react-router-dom"
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import { useEffect } from 'react';
 
 import { Loader } from "lucide-react";
@@ -15,6 +16,8 @@ import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  const { theme } = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -32,7 +35,7 @@ const App = () => {
 
 
   return (
-    <div>
+    <div data-theme= {theme}>
       <Navbar />
 
       <Routes>
