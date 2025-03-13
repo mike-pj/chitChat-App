b74908ffc330da6da127d906ed6b062fdc623059
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-03-13 17:15:28 +0100

    updated the path for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,15 +21,16 @@ mongoose
     console.log("Error connecting to DB");
   });
 
-const port = process.env.PORT;
+  
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(cors({
+    origin: "http://localhost:5173",
+    headers: { "Content-Type": "application/json" },
+    credentials: true,
+  }))
+  
 const __dirname = path.resolve();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -37,14 +38,13 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 };
 
 
-
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`server is running on port: ${port}`);
